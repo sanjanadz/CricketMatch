c041da0bb8f2fb9995c45316300050fc5d665570
Sanjanadz <h.sanjana1996@gmail.com>
2021-03-03 01:21:08 +0530

    Get Team names

--- Cricket/src/cricket/Cricket.java
+++ Cricket/src/cricket/Cricket.java
@@ -5,10 +5,7 @@
  */
 package cricket;
 
-/**
- *
- * @author User
- */
+import java.util.Scanner;
 public class Cricket {
 
     /**
@@ -16,6 +13,22 @@ public class Cricket {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    
+       String name1,name2 ="";
+       int wicket = 0;
+       int random, ball, score, over =0;
+       
+       Scanner s = new Scanner(System.in); 
+       
+        System.out.println("Enter Team 01 Name: ");
+        name1 = s.nextLine();
+        System.out.println("Enter Team 02 Name: ");
+        name2 = s.nextLine();
+       
+        System.out.println("Team 1: "+name1);
+        System.out.println("Team 2: "+name2);
+       
+        
     }
     
 }
